0e248167682d083c0ff47f06f5c1334bb40d31e0
ldevroye <louis.devroye@ulb.be>
2024-12-23 13:54:31 +0100

    [ADD] comments

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import random
 from math import ceil, sqrt
-from typing import Tuple, Any, List, Set
+from typing import Tuple, List
 from dataclasses import dataclass
 import time
 
+"""
+Description : Assignment for INFO-F413, Randomized algorithms, ULB.
+Compare Karger’s Algorithm and the FastCut algorithm.
 
+Author : Louis Devroye (523920) loudevroye@gmail.com
+Date : 23/12/2024
+VERSION: Python 3.12
+LICENSE: MIT
+"""
+
+
+# random seed (my id)
 SEED = 523920
+
 
 @dataclass
 class Graph:
-    edges: dict[int: List[int]]  # edges[1] = [2,3,5,8] connected nodes
+    """
+    a simple implementation of a possibly multigraph (several edges between two vertices)
+    """
+    edges: dict[int: List[int]]  # edges[1] = [2,3,5,8] edges between key and all the values
 
     def __init__(self, edges: dict):
         self.edges: dict[int: List[int]] = edges
@@ -29,7 +44,7 @@ class Graph:
         return f"\nEdges: {str_edges}, {self.num_edges} \nNodes : {self.nodes}, {self.num_nodes}"
 
     @property
-    def get_cut_size(self):
+    def cut_size(self) -> int:
         return self.num_edges
 
     @property
@@ -42,17 +57,27 @@ class Graph:
         for k in self.edges.values():
             result += len(k)
 
-        return result // 2
+        return result // 2  # by the handshaking lemma (1 edge contribute to two keys) we can //2
 
     @property
     def nodes(self) -> List[int]:
+        """
+        :return: a list of the vertices
+        """
         return list(self.edges.keys())
 
     @property
     def num_nodes(self) -> int:
+        """
+        :return: number of vertices in the graph
+        """
         return len(self.edges.keys())
 
     def add_node(self, k=1):
+        """
+        Add 'k' vertices to the graph
+        :param k: number of vertices to add
+        """
         n: int = 0
         if self.num_nodes > 0:
             n = max(self.edges.keys()) + 1
@@ -61,6 +86,11 @@ class Graph:
             self.edges[i] = []
 
     def add_edge(self, u: int, v: int):
+        """
+        Add an edge from u v (and from v to u since the graph is not directed)
+        :param u: 1st vertex
+        :param v: 2nd vertex
+        """
         if u not in self.edges:
             self.edges[u] = []
         if v not in self.edges:
@@ -70,6 +100,11 @@ class Graph:
         self.edges[v].append(u)
 
     def remove_edge(self, u, v):
+        """
+        remove the edge between two vertices (in both sides since the graph is not directed)
+        :param u: 1st vertex
+        :param v: 2nd vertex
+        """
         if u not in self.nodes and v not in self.nodes:
             raise ValueError(f"edge exception {u} or {v} not in graph")
 
@@ -79,13 +114,24 @@ class Graph:
             self.edges[v].remove(u)
 
     def get_random_edge(self) -> Tuple[int, int]:
+        """
+        :return: a tuple(u, v) corresponding to an edge
+        """
         u = random.choice(self.nodes)
         v = random.choice(self.edges[u])
         return u, v
 
     def contract(self, u, v):
+        """
+        contracts two neighboring vertices by putting all of v's neighbors in u and then deleting v
+        :param u: 1st vertex (the main target)
+        :param v: 2nd vertex (the one deleted)
+        """
+
         if u not in self.nodes or v not in self.nodes:
             raise ValueError(f"Both nodes {u} and {v} must exist in the graph.")
+        if u not in self.edges[v]:
+            raise ValueError(f"{u}, {v} not neighbors")
 
         # Merge edges of v into u, avoiding self-loops
         self.edges[u].extend(neighbor for neighbor in self.edges[v] if neighbor != u)
@@ -100,7 +146,10 @@ class Graph:
         # Remove v from the graph
         del self.edges[v]
 
-    def contract_random(self):
+    def contract_random_edge(self):
+        """
+        contract a random edge
+        """
         u, v = self.get_random_edge()
         #print("a: " + str(u) + ", b: " + str(v))
         #print(self.__str__())
@@ -115,35 +164,43 @@ class Solver:
         Karger's Contract algorithm for minimum cut using rustworkx.
 
         Returns:
-            int: the cut of the two vertices left
+            int: the cut (nbr of edges) of the two vertices left
         """
         # the final cut
-        return Solver.contract_until(graph, 2).num_edges
+        return Solver.contract_until(graph, 2).cut_size
 
     @staticmethod
     def contract_until(graph_to_contract: Graph, target_vertices: int) -> Graph:
-        """Perfom contractions on a copy of graph_to_contract until the graph has target_vertices left."""
+        """
+        Perfom contractions on a copy of graph_to_contract until the graph has target_vertices left.
+        :param graph_to_contract: the graph that will be copied
+        :param target_vertices: nmber of vertices to have left after
+        :return: the resulting graph
+        """
 
         result = graph_to_contract.copy()
         while result.num_nodes > target_vertices:
-            result.contract_random()
+            result.contract_random_edge()
 
         return result
 
     @staticmethod
     def fast_cut(graph: Graph, time_out: float = float('inf')) -> int:
         """
-        FastCut algorithm for minimum cut.
+        FastCut algorithm for minimum cut. if the time out is exceeded, one last pass in the loop and the function
+        returns the minimal cut computed
 
-        Returns:
+        :param graph: the graph that will be copied
+        :param time_out: the time out (in secondes) after that the excectuion stops and returns the min_cut computed
+        :Returns:
             int: The smallest cut.
         """
-        local_graph = graph.copy()
+        local_graph: Graph = graph.copy()
 
-        stack = [(local_graph, local_graph.num_nodes)]
+        stack: List[Tuple[Graph, int]] = [(local_graph, local_graph.num_nodes)]
         min_cut = float('inf')
 
-        begin = time.time()
+        begin: float = time.time()
         while stack and min_cut > 2 and (time.time() - begin < time_out):
             # 1.
             subgraph, n = stack.pop()
@@ -157,8 +214,8 @@ class Solver:
             t: int = ceil(1 + n / sqrt(2))
 
             # b
-            H1 = Solver.contract_until(subgraph, t)
-            H2 = Solver.contract_until(subgraph, t)
+            H1: Graph = Solver.contract_until(subgraph, t)
+            H2: Graph = Solver.contract_until(subgraph, t)
 
             # c
             stack.append((H1, t))
@@ -169,7 +226,15 @@ class Solver:
 
     @staticmethod
     def generate_random_graph(num_vertices, edge_probability=0.5) -> Graph:
-        """Generates a random undirected graph using networkx."""
+        """
+        Generate a partially random graph. This graph is 2-connected (if the implementation is correct,
+        but it is at least simply connected because of the dfs() search). This avoids unstable and uninteresting graphs.
+        :param num_vertices: the number of vertices the graph has
+        :param edge_probability: the probability for all the edges to exists. (only one-sided,
+        we add every edge (u, v) with u < v, except if the graph is not connected then we add random edges)
+
+        :return: the created graph
+        """
         result: Graph = Graph(dict())
         result.add_node(num_vertices)
 
@@ -250,7 +315,7 @@ def main_test():
 def main():
     #test parameters
     prob: float = 0
-    num_v: int = 10
+    num_v: int = 100
     number_try: int = 1000
     time_out: float = 10.0
 
@@ -282,36 +347,35 @@ def main():
     print(f"Summary of Results: {num_edges} edges - {num_edges/number_try} average")
     print(f"{error} errors ({error/number_try*100}%)")
 
-def main_2():
 
-    prob: float = 0.1
-    num_v = 50
+def basic_main():
+
+    prob: float = 0.5
+    num_v = 100
     # Generate a random graph
     start_time = time.time()
-    print(f"STARTING {prob} {num_v}")  #: {GRAPH}")
 
     GRAPH = Solver.generate_random_graph(num_v, prob)
-
-    #GRAPH.contract_random()
-    #print("AFTER : " + str(GRAPH))
+    print(f"STARTING {prob*100}%, {num_v} vertices, {GRAPH.num_edges} edges")
 
     # Run Karger's algorithm
     karger_cut = Solver.contract(GRAPH)
-    print(f"Karger's Algorithm Cut: {karger_cut}")
     second_time = time.time() - start_time
-    print("Time taken:", second_time, "seconds")
+
+    print(f"Karger's Algorithm Cut: {karger_cut}")
+    print(f"Time taken: {second_time:0.4f} seconds")
 
     # Run FastCut algorithm
-    fast_cut_result = Solver.fast_cut(GRAPH, ceil(10 + 1) ** 3)
-    print("FastCut Algorithm Cut:", fast_cut_result)
+    fast_cut_result = Solver.fast_cut(GRAPH, 120)
     third_time = time.time() - start_time
-    print("Time taken:", third_time, "seconds")
+
+    print("FastCut Algorithm Cut:", fast_cut_result)
+    print(f"Time taken: {third_time:0.4f} seconds")
 
 
 if __name__ == "__main__":
     random.seed(SEED)
-    #main_test()
-    main()
+    basic_main()
 
 
 
